26b2f749e82f2774a8b7e2eb174bb113e83b32d1
nogusdn <97591002+nogusdn@users.noreply.github.com>
2022-09-07 16:00:15 +0900

    Feat: 회원가입, 로그인 기능

--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -1,29 +1,114 @@
 import styled from "styled-components";
+import TextField from "@mui/material/TextField";
+import { FcGoogle } from "react-icons/fc";
 
-// const image = process.env.PUBLIC_URL + `/assets/1.png`;
+import { useNavigate } from "react-router-dom";
 
-const LoginDiv = styled.div`
+const LoginContainer = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const LoginBox = styled.div`
+  display: flex;
+  width: 70vw;
+  height: 70vh;
+  border: 1px solid #ececec;
 `;
 
 const LeftDiv = styled.div`
-  display: flex;
-  justify-content: start;
-  width: 100%;
+  width: 35vw;
 `;
-
 const RightDiv = styled.div`
   display: flex;
-  justify-content: start;
-  width: 100%;
+  flex-direction: column;
+
+  align-items: center;
+  width: 35vw;
+`;
+
+const InputDiv = styled.div`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const SButton = styled.button`
+  border-radius: 20px;
+  color: white;
+  border: none;
+  background-color: #42a5f5;
+  padding: 5px;
+  margin-top: 40px;
+  width: 50%;
+`;
+
+const IconDiv2 = styled.div`
+  margin: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  border-top: 1px solid #ececec;
+`;
+
+const IconDiv3 = styled.div`
+  margin: 5px;
+  font-size: 20px;
+  cursor: pointer;
+`;
+
+const StyledP = styled.p`
+  font-size: 12px;
+  cursor: pointer;
+`;
+
+const FooterDiv = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 `;
 
 const Login = () => {
+  const navigate = useNavigate();
   return (
-    <LoginDiv>
-      <LeftDiv>L</LeftDiv>
-      <RightDiv>R</RightDiv>
-    </LoginDiv>
+    <LoginContainer>
+      <LoginBox>
+        <LeftDiv>L</LeftDiv>
+        <RightDiv>
+          <h1>아Eng</h1>
+          <InputDiv>
+            <TextField
+              fullWidth
+              id="standard-basic"
+              label="ID(email)"
+              variant="standard"
+            />
+          </InputDiv>
+          <InputDiv>
+            <TextField
+              fullWidth
+              id="standard-basic"
+              label="Password"
+              variant="standard"
+            />
+          </InputDiv>
+          <SButton>로그인</SButton>
+          <IconDiv2>
+            <IconDiv3>
+              <FcGoogle />
+            </IconDiv3>
+            <p> Google로 로그인</p>
+          </IconDiv2>
+          <FooterDiv>
+            <StyledP>비밀번호 찾기</StyledP>
+            <StyledP onClick={() => navigate("/signup")}>회원가입</StyledP>
+          </FooterDiv>
+        </RightDiv>
+      </LoginBox>
+    </LoginContainer>
   );
 };
 
--- frontend/src/pages/Auth/SignUp.js
+++ frontend/src/pages/Auth/SignUp.js
@@ -7,11 +7,18 @@ import { GrLinkPrevious } from "react-icons/gr";
 import { FcGoogle } from "react-icons/fc";
 import { Toast } from "../../assets/Toast";
 
-const SignUpDiv = styled.div`
+const SignUpContainer = styled.div`
   display: flex;
-  justify-content: start;
+  justify-content: center;
   align-items: center;
   height: 100vh;
+`;
+
+const SingUpBox = styled.div`
+  display: flex;
+  width: 70vw;
+  height: 70vh;
+  border: 1px solid #ececec;
 `;
 
 const LeftDiv = styled.div`
@@ -27,7 +34,6 @@ const RightDiv = styled.div`
   justify-content: start;
   align-items: center;
   width: 100%;
-  border: 1px solid #ececec;
   padding: 10px;
 `;
 
@@ -158,104 +164,57 @@ const SignUp = () => {
     });
 
     formData.append("data", blob);
+    formData.append("profile", profile);
   };
 
   return (
-    <SignUpDiv>
-      <LeftDiv>L</LeftDiv>
-      {page === 1 ? (
-        <RightDiv>
-          <Sdiv>
-            <h1>아Eng</h1>
-          </Sdiv>
-          <InputDiv>
-            <TextField
-              fullWidth
-              id="standard-basic"
-              label="ID (email)"
-              variant="standard"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <SButton2>인증번호 보내기</SButton2>
-            <TextField
-              fullWidth
-              id="standard-basic"
-              label="인증번호 입력"
-              variant="standard"
-              value={emailCheck}
-              onChange={(e) => setEmailCheck(e.target.value)}
-            />
-
-            <TextField
-              fullWidth
-              id="standard-basic"
-              label="password"
-              type="password"
-              variant="standard"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <TextField
-              fullWidth
-              id="standard-basic"
-              label="password check"
-              type="password"
-              variant="standard"
-              value={passwordCheck}
-              onChange={(e) => setPasswordCheck(e.target.value)}
-            />
-            <IconDiv onClick={handlePage}>
-              <GrLinkNext />
-            </IconDiv>
-            <IconDiv2>
-              <IconDiv3>
-                <FcGoogle />
-              </IconDiv3>
-              <p> Google로 로그인</p>
-            </IconDiv2>
-          </InputDiv>
-        </RightDiv>
-      ) : (
-        <>
+    <SignUpContainer>
+      <SingUpBox>
+        <LeftDiv>L</LeftDiv>
+        {page === 1 ? (
           <RightDiv>
             <Sdiv>
               <h1>아Eng</h1>
             </Sdiv>
             <InputDiv>
-              <ProfileContainer>
-                <ProfileBox>
-                  <Profileimg src={preview} alt="" />
-                </ProfileBox>
-                <input
-                  type="file"
-                  name="file"
-                  onChange={(e) => {
-                    changeImg(e);
-                    encodeFileToBase64(e.target.files[0]);
-                  }}
-                />
-              </ProfileContainer>
               <TextField
                 fullWidth
                 id="standard-basic"
-                label="닉네임"
+                label="ID (email)"
                 variant="standard"
-                value={nickname}
-                onChange={(e) => setNickname(e.target.value)}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
+              <SButton2>인증번호 보내기</SButton2>
               <TextField
                 fullWidth
                 id="standard-basic"
-                label="생년월일"
+                label="인증번호 입력"
                 variant="standard"
-                value={birth}
-                onChange={(e) => setBirth(e.target.value)}
+                value={emailCheck}
+                onChange={(e) => setEmailCheck(e.target.value)}
               />
 
+              <TextField
+                fullWidth
+                id="standard-basic"
+                label="password"
+                type="password"
+                variant="standard"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <TextField
+                fullWidth
+                id="standard-basic"
+                label="password check"
+                type="password"
+                variant="standard"
+                value={passwordCheck}
+                onChange={(e) => setPasswordCheck(e.target.value)}
+              />
               <IconDiv onClick={handlePage}>
-                <GrLinkPrevious />
-                <SButton onClick={handleSubmit}>회원가입</SButton>
+                <GrLinkNext />
               </IconDiv>
               <IconDiv2>
                 <IconDiv3>
@@ -265,9 +224,59 @@ const SignUp = () => {
               </IconDiv2>
             </InputDiv>
           </RightDiv>
-        </>
-      )}
-    </SignUpDiv>
+        ) : (
+          <>
+            <RightDiv>
+              <Sdiv>
+                <h1>아Eng</h1>
+              </Sdiv>
+              <InputDiv>
+                <ProfileContainer>
+                  <ProfileBox>
+                    <Profileimg src={preview} alt="" />
+                  </ProfileBox>
+                  <input
+                    type="file"
+                    name="file"
+                    onChange={(e) => {
+                      changeImg(e);
+                      encodeFileToBase64(e.target.files[0]);
+                    }}
+                  />
+                </ProfileContainer>
+                <TextField
+                  fullWidth
+                  id="standard-basic"
+                  label="닉네임"
+                  variant="standard"
+                  value={nickname}
+                  onChange={(e) => setNickname(e.target.value)}
+                />
+                <TextField
+                  fullWidth
+                  id="standard-basic"
+                  label="생년월일"
+                  variant="standard"
+                  value={birth}
+                  onChange={(e) => setBirth(e.target.value)}
+                />
+
+                <IconDiv onClick={handlePage}>
+                  <GrLinkPrevious />
+                  <SButton onClick={handleSubmit}>회원가입</SButton>
+                </IconDiv>
+                <IconDiv2>
+                  <IconDiv3>
+                    <FcGoogle />
+                  </IconDiv3>
+                  <p> Google로 로그인</p>
+                </IconDiv2>
+              </InputDiv>
+            </RightDiv>
+          </>
+        )}
+      </SingUpBox>
+    </SignUpContainer>
   );
 };
 
