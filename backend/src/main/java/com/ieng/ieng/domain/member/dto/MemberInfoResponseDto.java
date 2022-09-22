c8105ecb30ebcabb93d428a49c4404f5d915ad06
DESKTOP-KVCQHCD\multicampus <inseok9876@gmail.com>
2022-09-22 16:45:17 +0900

    Feat: 회원정보 조회 구현

--- backend/src/main/java/com/ieng/ieng/domain/member/dto/MemberInfoResponseDto.java
+++ backend/src/main/java/com/ieng/ieng/domain/member/dto/MemberInfoResponseDto.java
@@ -1,9 +1,12 @@
 package com.ieng.ieng.domain.member.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ieng.ieng.domain.member.entity.Member;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,9 +15,18 @@ public class MemberInfoResponseDto {
 
     private String nickname;
     private String email;
+    @JsonProperty("birth_YMD")
+    @JsonFormat(shape= JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd")
+    private Date memberYMD;
+    private String provider;
+    private String picturePath;
+
     @Builder
     public MemberInfoResponseDto(Member entity){
         this.nickname = entity.getNickname();
         this.email = entity.getEmail();
+        this.memberYMD=entity.getMemberYMD();
+        this.provider=entity.getProvider();
+        this.picturePath= entity.getPicturePath();
     }
 }
