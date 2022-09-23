21fc4c0eefd73425be8c50524dee0a5c1149b474
류인석 <inseok9876@gmail.com>
2022-09-23 23:41:36 +0900

    Feat: 단어문장 히스토리 조회기능 구현.

--- backend/src/main/java/com/ieng/ieng/domain/history/repository/SentenceHistoryRepository.java
+++ backend/src/main/java/com/ieng/ieng/domain/history/repository/SentenceHistoryRepository.java
@@ -1,6 +1,7 @@
 package com.ieng.ieng.domain.history.repository;
 
 import com.ieng.ieng.domain.history.entity.SentenceHistory;
+import com.ieng.ieng.domain.history.entity.WordHistory;
 import com.ieng.ieng.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,6 @@ import java.util.List;
 public interface SentenceHistoryRepository extends JpaRepository<SentenceHistory, Long> {
 
     List<SentenceHistory> findSentenceHistoriesByMember(Member member);
+    List<SentenceHistory> findSentenceHistoriesByMemberAndSentenceHistoryDTTM(Member member, String sentenceHistoryDTTM);
+
 }
