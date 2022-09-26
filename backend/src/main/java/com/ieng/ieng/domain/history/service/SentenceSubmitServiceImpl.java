bb0cb4f988e96cc33564861bc76f278da583bcc7
류인석 <inseok9876@gmail.com>
2022-09-27 00:26:57 +0900

    Feat: 구글 로그인 구현

--- backend/src/main/java/com/ieng/ieng/domain/history/service/SentenceSubmitServiceImpl.java
+++ backend/src/main/java/com/ieng/ieng/domain/history/service/SentenceSubmitServiceImpl.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.List;
 
 @Service
