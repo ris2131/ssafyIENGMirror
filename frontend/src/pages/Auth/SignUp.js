e7c8f2b7c82f341e6c48d716f83b39e9b2ca3f88
nogusdn <97591002+nogusdn@users.noreply.github.com>
2022-09-30 16:10:22 +0900

    Feat: 이메일 중복체크, 인증, 타이머 구현

--- frontend/src/pages/Auth/SignUp.js
+++ frontend/src/pages/Auth/SignUp.js
@@ -1,18 +1,27 @@
 import { useState, useRef } from "react";
+import { useDispatch } from "react-redux";
 
-import styled from "styled-components";
-import TextField from "@mui/material/TextField";
-import { mobileback } from "../../assets/BackgroundImg";
-import { GrLinkNext } from "react-icons/gr";
-import { GrLinkPrevious } from "react-icons/gr";
-import { FcGoogle } from "react-icons/fc";
-import { TbCameraPlus } from "react-icons/tb";
+import {
+  checkEmail,
+  confirmEmail,
+  sendEmail,
+  signup,
+} from "../../redux/AuthSlice";
 import { Toast } from "../../assets/Toast";
 
+import GoogleTest from "../OauthLogin/GoogleTest";
+
+// style
+import styled from "styled-components";
+import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
-import { emailAuth, signup } from "../../redux/AuthSlice";
-import { useDispatch } from "react-redux";
+import { GrLinkNext } from "react-icons/gr";
+import { GrLinkPrevious } from "react-icons/gr";
+import { TbCameraPlus } from "react-icons/tb";
+import { mobileback } from "../../assets/BackgroundImg";
+import Swal from "sweetalert2";
+import Timer from "./Timer";
 
 const backgroundImage = process.env.PUBLIC_URL + `/assets/background2.jpg`;
 
@@ -58,11 +67,33 @@ const LogoText = styled.h1`
   margin: 10px;
 `;
 
+const FlexInputDiv = styled.div`
+  @media screen and (max-width: 821px) {
+    width: 50vw;
+  }
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 20vw;
+  margin-bottom: 10px;
+  cursor: pointer;
+`;
+
+const CheckButton = styled.button`
+  margin-top: 20px;
+  border: none;
+  border-radius: 10px;
+  background-color: ${(props) => props.back};
+  padding: 5px;
+  color: white;
+  width: 100px;
+  cursor: ${(props) => props.pointer};
+`;
+
 const InputDiv = styled.div`
   @media screen and (max-width: 821px) {
     width: 50vw;
   }
-
   width: 20vw;
   width: ${(props) => props.width};
   margin-bottom: 10px;
@@ -75,20 +106,6 @@ const IconDiv = styled.div`
   align-items: center;
   cursor: pointer;
 `;
-const IconDiv2 = styled.div`
-  margin: 30px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 30%;
-  border-top: 1px solid #ececec;
-`;
-
-const IconDiv3 = styled.div`
-  margin: 5px;
-  font-size: 20px;
-  cursor: pointer;
-`;
 
 const SButton = styled.button`
   @media screen and (max-width: 821px) {
@@ -99,7 +116,7 @@ const SButton = styled.button`
   color: white;
   border: none;
   background-color: #42a5f5;
-  padding: 10px;
+  padding: 8px;
   margin: 10px 0px;
   width: 20vw;
   cursor: pointer;
@@ -155,6 +172,18 @@ const SignUp = () => {
   const [year, setYear] = useState("");
   const [month, setMonth] = useState("월");
   const [day, setDay] = useState("");
+  const [emailPass, setEmailPass] = useState(false);
+  const [timeOn, setTimeOn] = useState(true);
+
+  // 타이머
+  // const [min, setMin] = useState(5);
+  // const [sec, setSec] = useState(0);
+  // const time = useRef(300);
+  // const timerId = useRef(null);
+
+  // 이메일 유효성 확인
+  const [emailStatus, setEmailStatus] = useState(false);
+
   const monthList = [...Array(12)].map((v, i) => i + 1);
   const date = ["월", ...monthList];
 
@@ -164,9 +193,14 @@ const SignUp = () => {
   const dispatch = useDispatch();
 
   const handlePage = () => {
-    if (page === 1) {
-      setPage(2);
-    } else setPage(1);
+    if (!emailPass) {
+      Swal.fire({ icon: "error", title: "이메일 인증이 필요해요!" });
+      return;
+    } else {
+      if (page === 1) {
+        setPage(2);
+      } else setPage(1);
+    }
   };
 
   const changeImg = (e) => {
@@ -258,13 +292,81 @@ const SignUp = () => {
     // formData.append("data", blob);
     // formData.append("profile", profile);
   };
-  const handleEmail = () => {
-    dispatch(emailAuth(email))
+
+  // 이메일 형식 체크 함수
+  const handleEmailCheck = (email) => {
+    const emailRegex =
+      /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    const check = !emailRegex.test(email) ? false : true;
+    setEmailStatus(check);
+  };
+
+  // 인증번호 받기
+
+  const handleSendMail = () => {
+    const data = {
+      email,
+    };
+    dispatch(sendEmail(data))
+      .unwrap()
       .then((res) => console.log(res))
       .catch((err) => console.error(err));
   };
 
-  // useEffect(() => (page === 2 ? nicknameRef.current.focus() : null), [page]);
+  const getCode = () => {
+    if (!emailStatus) {
+      Swal.fire({ icon: "error", title: "이메일 형식이 올바르지 않습니다" });
+      return;
+    }
+    const data = {
+      email,
+    };
+    dispatch(checkEmail(data))
+      .unwrap()
+      .then((res) => {
+        if (res.status === "SUCCESS") {
+          handleSendMail();
+          setTimeOn(true);
+        } else Swal.fire({ icon: "error", title: "이미 가입한 이메일입니다." });
+      })
+      .catch((err) => console.error(err));
+  };
+
+  // 인증번호 확인
+  const confirmCode = () => {
+    const data = {
+      certification_number: emailCheck,
+    };
+    console.log(data);
+
+    dispatch(confirmEmail(data))
+      .unwrap()
+      .then(() => {
+        Swal.fire({ icon: "success", title: "인증이 완료되었습니다." });
+        setEmailPass(true);
+        setTimeOn(false);
+      })
+      .catch(() => {
+        Swal.fire({ icon: "error", title: "인증번호를 다시 확인해주세요." });
+        setEmailPass(false);
+      });
+  };
+
+  // useEffect(() => {
+  //   timerId.current = setInterval(() => {
+  //     setMin(parseInt(time.current / 60));
+  //     setSec(time.current % 60);
+  //     time.current -= 1;
+  //   }, 1000);
+  // }, []);
+
+  // useEffect(() => {
+  //   if (time.current <= 0) {
+  //     window.alert("인증시간이 초과하였습니다.");
+  //     clearInterval(timerId.current);
+  //   }
+  //   return () => clearInterval(timerId.current);
+  // }, [sec]);
 
   return (
     <SingUpBox>
@@ -280,11 +382,20 @@ const SignUp = () => {
               label="ID (email)"
               variant="standard"
               value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={(e) => {
+                setEmail(e.target.value);
+                handleEmailCheck(e.target.value);
+              }}
             />
+            <SButton
+              onClick={() => {
+                getCode();
+              }}
+            >
+              인증코드 받기
+            </SButton>
           </InputDiv>
-          <InputDiv>
-            <SButton onClick={handleEmail}>인증번호 보내기</SButton>
+          <FlexInputDiv>
             <TextField
               fullWidth
               label="인증번호 입력"
@@ -292,7 +403,19 @@ const SignUp = () => {
               value={emailCheck}
               onChange={(e) => setEmailCheck(e.target.value)}
             />
-          </InputDiv>
+            {timeOn ? <Timer setTimeOn={setTimeOn} /> : null}
+            {emailPass ? (
+              <CheckButton back="#bdbdbd">완료</CheckButton>
+            ) : (
+              <CheckButton
+                onClick={confirmCode}
+                pointer="pointer"
+                back="#42a5f5"
+              >
+                확인
+              </CheckButton>
+            )}
+          </FlexInputDiv>
           <InputDiv>
             <TextField
               fullWidth
@@ -318,12 +441,7 @@ const SignUp = () => {
           <IconDiv>
             <GrLinkNext onClick={handlePage} />
           </IconDiv>
-          <IconDiv2>
-            <IconDiv3>
-              <FcGoogle />
-            </IconDiv3>
-            <p> Google로 로그인</p>
-          </IconDiv2>
+          <GoogleTest />
         </>
       ) : (
         <>
